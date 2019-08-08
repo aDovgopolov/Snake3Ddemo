2a2487e87087f9383ee135d4c05e214e432ebd2d
aDovgopolov <dn070291dag@gmail.com>
2019-08-08 18:02:05 +0300

    snake move

---
+++ Snake3demo/Assets/Scripts/Grid.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public static int x = 15;
+    public static int y = 15;
+    public static int z = 15;
+    public static Transform[,] grid = new Transform[x, y];
+    public static Transform[,,] grid3D = new Transform[x, y, z];
+
+    public static Vector2 RoundVec2(Vector2 v)
+    {
+        return new Vector2(Mathf.Round(v.x),
+            Mathf.Round(v.y));
+    }
+    
+    public static bool InsideBorder(Vector2 pos)
+    {
+        return ( (int)pos.x >= 0 && (int)pos.x < x && (int)pos.y >= 0 );
+    }
+    
+    public static void UpdateGrid(Snake group)
+    {
+        for (int y = 0; y < Grid.y; ++y)
+        for (int x = 0; x < Grid.x; ++x)
+            if (Grid.grid[x, y] != null)
+                if (Grid.grid[x, y].parent == group.gameObject.transform)
+                    Grid.grid[x, y] = null;
+
+        foreach (Transform child in group.gameObject.transform)
+        {
+            Vector2 v = Grid.RoundVec2(child.position);
+            Grid.grid[(int)v.x, (int)v.y] = child;
+        }
+    }
+    
+    public static bool IsValidGridPos(Snake sname)
+    {
+        foreach (Transform child in sname.gameObject.transform)
+        {
+            Vector2 v = Grid.RoundVec2(child.position);
+
+            if (!Grid.InsideBorder(v))
+                return false;
+
+            if (Grid.grid[(int)v.x, (int)v.y] != null &&
+                Grid.grid[(int)v.x, (int)v.y].parent != sname.gameObject.transform)
+                return false;
+        }
+        return true;
+    }
+    
+    //----------------------------------------------------------------//
+    public static Vector3 RoundVec3(Vector3 v)
+    {
+        return new Vector3(Mathf.Round(v.x),
+            Mathf.Round(v.y),
+            Mathf.Round(v.z));
+    }
+    
+    public static bool InsideBorder3D(Vector3 pos)
+    {
+        bool inX = (int) pos.x >= 0 && (int) pos.x < x;
+        bool inY = (int) pos.y >= 0 && (int) pos.y < y;
+        bool inZ = (int) pos.z >= 0 && (int) pos.z < z;
+        return ( inX && inY && inZ );
+    }
+    
+    public static void UpdateGrid3D(Snake group)
+    {
+        for (int y = 0; y < Grid.y; ++y)
+            for (int x = 0; x < Grid.x; ++x)
+                for (int z = 0; z < Grid.x; ++z)
+                    if (Grid.grid3D[x, y, z] != null)
+                        if (Grid.grid3D[x, y, z].parent == group.gameObject.transform)
+                            Grid.grid3D[x, y, z] = null;
+
+        foreach (Transform child in group.gameObject.transform)
+        {
+            Vector3 v = Grid.RoundVec3(child.position);
+            Grid.grid3D[(int)v.x, (int)v.y, (int)v.z] = child;
+        }
+    }
+    
+    public static bool IsValidGridPos3D(Snake sname)
+    {
+        foreach (Transform child in sname.gameObject.transform)
+        {
+            Vector3 v = Grid.RoundVec3(child.position);
+
+            if (!Grid.InsideBorder3D(v))
+                return false;
+
+            if (Grid.grid3D[(int)v.x, (int)v.y, (int)v.y] != null &&
+                Grid.grid3D[(int)v.x, (int)v.y, (int)v.y].parent != sname.gameObject.transform)
+                return false;
+        }
+        return true;
+    }
+    
+    
+    //public static 
+}
---
+++ Snake3demo/Assets/Scripts/Snake.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using Random = System.Random;
+
+public class Snake : MonoBehaviour
+{
+    private float lastMove = 1;
+
+    private Vector3[] directions = new Vector3[]
+          {
+              new Vector3(0, -1, 0), 
+              new Vector3(0, 1, 0), 
+              new Vector3(-1, 0, 0), 
+              new Vector3(1, 0, 0), 
+              new Vector3(0, 0, 1)
+              
+          };
+
+    void Update()
+    {
+        if (Time.time - lastMove >= 1)
+        {
+            Movement();
+        }
+    }
+
+    public void Movement()
+    {
+        Vector3 direction = CheckFreeSpaceExeptItself();
+        
+        Vector3 oldPosition  = transform.GetChild(0).transform.position;
+        transform.GetChild(0).transform.position += direction;
+        
+        
+        for (int i = 1; i < transform.GetChildCount(); i++)
+        {
+            Transform childPos = transform.GetChild(i).transform;
+            Vector3 childOldPos = childPos.position;
+            childPos.position = oldPosition;
+            oldPosition = childOldPos;
+        }
+        
+        
+        
+        /*if (Grid.IsValidGridPos3D(this))
+        {
+            Grid.UpdateGrid3D(this);
+            Debug.Log("IsValidGridPos3D");
+        }
+        else
+        {
+            transform.position = oldPosition;
+            Debug.Log("NO IsValidGridPos3D");
+            // transform.position += new Vector3(1, 0, 0);
+        }*/
+        
+        lastMove = Time.time;
+    }
+
+
+    public Vector3 CheckFreeSpaceExeptItself()
+    {
+        Vector3 direction = gameObject.transform.GetChild(0).localPosition;
+        Random random = new Random();
+        bool isFreeSpace = false;
+        
+        while (!isFreeSpace)
+        {
+            direction += directions[random.Next(0, directions.Length) ];
+            
+            //костыль какой-то
+            foreach (Transform child in gameObject.transform)
+            {
+                Debug.Log($"direction = {direction}  - child.position = {child.position}");
+                if (direction != child.position)
+                {
+                    isFreeSpace = true;
+                    break;
+                }
+                else
+                {
+                    direction = gameObject.transform.GetChild(0).localPosition;
+                    direction +=  directions[random.Next(0, directions.Length) ];
+                }
+            }
+        }
+
+        return direction;
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        MeshRenderer meshRend = GetComponent<MeshRenderer>();
+        meshRend.material.color = Color.green;
+        Debug.Log(other.name);
+    }
+
+    public void Brak()
+    { 
+        /*foreach (Transform child in transform)
+        {    
+            child.position += direction;
+            oldPosition = child.position;
+            
+        }*/
+        
+        
+        /*if (oldPosition != transform.position)
+        {
+            MoveTail(oldPosition);
+        }*/
+        
+    }
+}
